92c9583e9b764e672c322f13885bd94a781a68d6
pravin <Pravinnagpure633@gmail.com>
2023-04-10 10:05:43 +0530

    inital commit

---
+++ src/main/java/com/ashokit/GitBranchesApplication.java
@@ -0,0 +1,14 @@
+package com.ashokit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitBranchesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitBranchesApplication.class, args);
+		System.out.println("Hey");
+	}
+
+}
